f02a95fd00dd578cd4f3368b57db3b54e577d740
unknown <bittufardeen@gmail.com>
2022-07-01 22:29:18 +0530

    Assignment8PracticeProblem

---
+++ empwage/Employee.java
@@ -0,0 +1,26 @@
+
+
+public class Employee {
+    public static void main(String[] args) {
+
+        int isFullTime=1;
+        int isPartTime=2;
+        int EmpHrs=0;
+        int EmpWage=0;
+        int EmpRatPerHr=20;
+        int TotalDayWage=20;
+
+        double EmpCheck= Math.floor(Math.random()*10)%2;
+        if(EmpCheck == isFullTime)
+        {
+            EmpHrs=8;
+        }
+        else if (EmpCheck == isPartTime)
+        {
+            EmpHrs=4;
+        }
+        EmpWage = EmpHrs * EmpRatPerHr *TotalDayWage;
+        System.out.println("TotalDay Wage" +EmpWage);
+    }
+
+}
